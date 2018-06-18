836155b3f243bd649bc1ac65ea38278a810f638a
AlexMorreale <alexmorreale@me.com>
2018-06-18 18:28:06 -0400

    add better logging

--- serve.go
+++ serve.go
@@ -1,9 +1,9 @@
 /*
-  Serve is a very simple static file server in go
+Serve is a very simple static file server in go
 
-  Usage:
-  -p="8100": port to serve on
-  -d=".":    the directory of static files to host
+Usage:
+-p="8100": port to serve on
+-d=".":    the directory of static files to host
 */
 package main
 
@@ -13,13 +13,27 @@ import (
   "net/http"
 )
 
+func logger(h http.Handler) http.Handler {
+  return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+    log.Printf("%s requested %s", r.RemoteAddr, r.URL)
+    h.ServeHTTP(w, r)
+  })
+}
+
 func main() {
   port := flag.String("p", "3333", "port to serve on")
   directory := flag.String("d", "files", "the directory of static file to host")
   flag.Parse()
 
-  http.Handle("/", http.FileServer(http.Dir(*directory)))
+  h := http.NewServeMux()
+
+  h.Handle("/", http.FileServer(http.Dir(*directory)))
+
+  hl := logger(h)
+
 
   log.Printf("Serving %s on HTTP port: %s\n", *directory, *port)
-  log.Fatal(http.ListenAndServe(":"+*port, nil))
+  err := http.ListenAndServe(":"+*port, hl)
+
+  log.Fatal(err)
 }
